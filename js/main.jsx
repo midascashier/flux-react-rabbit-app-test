1fc56fea9dd29ac13cdaeddad397c57ab6b0b675
jocampo <jocampoz@gmail.com>
2016-05-18 08:19:05 -0600

    quitando query string

--- js/main.jsx
+++ js/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute  } from 'react-router'
+import { Router, Route, hashHistory, IndexRoute, useRouterHistory   } from 'react-router'
 import App from './components/App'
 import Wait from './components/wait'
 import Increase from './components/increase'
+import { createHashHistory } from 'history';
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
 
 render((
-    <Router history={hashHistory}>
+    <Router history={appHistory}>
         <Route path="/" component={App}>
             <Route path="/wait" component={Wait}/>
             <Route path="/increase" component={Increase}/>
